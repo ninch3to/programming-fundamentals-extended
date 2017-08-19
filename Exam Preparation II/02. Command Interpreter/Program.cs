b0ae605ebc7d2fc0edd3d1b218995ed505d568d5
Nina Varbanova <31161278+ninch3to@users.noreply.github.com>
2017-08-19 17:32:23 +0300

    Add files via upload

---
+++ Exam Preparation II/02. Command Interpreter/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Command_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            string[] command = Console.ReadLine().Split(' ');
+
+            while (command[0] != "end")
+            {
+                if (command[0] == "reverse")
+                {
+
+                }
+                else if (command[0] == "sort")
+                {
+
+                }
+                else if (command[0] == "rollLeft")
+                {
+                    int count = int.Parse(command[1]);
+                    string[] rotatedLeft = new string[0];
+                    for (int i = 0; i < count; i++)
+                    {
+                        rotatedLeft = RotateLeft(data);
+                        data = rotatedLeft;
+                    }
+                    Console.WriteLine("[" + string.Join(", ", rotatedLeft) + "]");
+                }
+                else
+                {
+                    int count = int.Parse(command[1]);
+                    string[] rotatedRight = new string[0];
+                    for (int i = 0; i < count; i++)
+                    {
+                        rotatedRight = RotateRight(data);
+                        data = rotatedRight;
+                    }
+                    Console.WriteLine("[" + string.Join(", ", rotatedRight) + "]");
+                }
+
+                command = Console.ReadLine().Split(' ');
+            }
+        }
+
+        private static string[] RotateLeft(string[] data)
+        {
+            string[] rotatedLeft = new string[data.Length];
+            for (int j = 0; j < data.Length - 1; j++)
+            {
+                rotatedLeft[j] = data[j + 1];
+            }
+            var firstElement = data[0];
+            rotatedLeft[data.Length - 1] = firstElement;
+            return rotatedLeft;
+        }
+
+        private static string[] RotateRight(string[] data)
+        {
+            string[] rotatedRight = new string[data.Length];
+            for (int j = 0; j < data.Length - 1; j++)
+            {
+                rotatedRight[j + 1] = data[j];
+            }
+            var lastElement = data[rotatedRight.Length - 1];
+            rotatedRight[0] = lastElement;
+            return rotatedRight;
+        }
+
+       
+    }
+}
---
+++ Exam Preparation II/1. SoftUni Coffee Orders/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.SoftUni_Coffee_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0m;
+
+            for (int i = 0; i < n; i++)
+            {
+                decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+                string[] orderDate = Console.ReadLine().Split('/');
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                int year = int.Parse(orderDate[2]);
+                int month = int.Parse(orderDate[1]);
+                int daysInMonth = DateTime.DaysInMonth(year, month);
+
+                decimal coffeePrice = (decimal)((daysInMonth * capsulesCount) * pricePerCapsule);
+                Console.WriteLine("The price for the coffee is: ${0:f2}", coffeePrice);
+                sum += coffeePrice;
+            }
+
+            Console.WriteLine("Total: ${0:f2}", sum);
+        }
+    }
+}
