ded279b541750a0f2066373fc2558398b248ddee
Nina Varbanova <31161278+ninch3to@users.noreply.github.com>
2017-08-19 17:21:09 +0300

    Add files via upload

---
+++ Arrays and Methods - 16.08.2017 - I/15. Count of Capital Letters in Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Count_of_Capital_Letters_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string values = Console.ReadLine();
+            string[] items = values.Split(' ');
+            string[] arr = new string[items.Length];
+
+            int count = 0;
+            for (int i = 0; i < items.Length; i++)
+            {
+                arr[i] = items[i];
+                if (arr.Length <= 1 && StartsWithUpper(arr[i]))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+        static bool StartsWithUpper(string values)
+        {
+            return Enumerable.Range(65, 26).Contains((int)values[0]);
+        }
+
+    }
+}
---
+++ Arrays and Methods - 16.08.2017 - I/16. Array Symmetry/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.Array_Symmetry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string values = Console.ReadLine();
+
+
+
+
+            string[] items = values.Split(' ');
+            string[] arr = new string[items.Length];
+            string str = "";
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                arr[i] = items[i];
+                str += items[i];
+            }
+            char[] newStr = str.ToArray();
+            
+            //Console.WriteLine(newStr);
+            Array.Reverse(arr);
+
+           
+
+          //  Console.WriteLine(newStr);
+        }
+    }
+}
