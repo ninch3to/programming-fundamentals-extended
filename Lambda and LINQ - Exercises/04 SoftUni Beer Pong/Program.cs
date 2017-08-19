32b789db238c85b6e2daf97a4996f894fab03fad
Nina Varbanova <31161278+ninch3to@users.noreply.github.com>
2017-08-19 17:37:06 +0300

    Add files via upload

---
+++ Lambda and LINQ - Exercises/04 SoftUni Beer Pong/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_SoftUni_Beer_Pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Dictionary<string, Dictionary<string, int>>();
+            string input = Console.ReadLine();
+            
+            while (input != "stop the game")
+            {
+                string[] inputTokens = input.Split('|');
+                string teamName = inputTokens[1];
+                string playerName = inputTokens[0];
+                int playerScore = int.Parse(inputTokens[2]);
+                
+                if (!data.ContainsKey(teamName))
+                {
+                    data.Add(teamName, new Dictionary<string, int>());
+                }
+
+                if (data[teamName].Count() < 3)
+                {
+                    data[teamName][playerName] = playerScore;
+                }
+
+                input = Console.ReadLine();
+            }
+            var orderedData = data
+              .Where(r => r.Value.Count == 3)
+              .OrderByDescending(r => r.Value.Sum(playerRecord => playerRecord.Value));
+
+            int teamRanking = 1;
+            foreach (var record in orderedData)
+            {
+                string key = record.Key;
+                var innerRecords = record.Value.OrderByDescending(d => d.Value);
+
+                Console.WriteLine("{0}. {1}; Players:", teamRanking, key);
+
+                foreach (var innerRecord in innerRecords)
+                {
+                    string innerKey = innerRecord.Key;
+                    int innerValue = innerRecord.Value;
+
+                    Console.WriteLine("###{0}: {1}", innerRecord.Key, innerRecord.Value);
+                }
+                teamRanking++;
+            }
+
+        }
+    }
+}
---
+++ Lambda and LINQ - Exercises/camping/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace camping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var campingData = new Dictionary<string, Dictionary<string, int>>();
+
+            string[] inputTokens = Console.ReadLine().Split(' ').ToArray();
+
+            while (inputTokens[0] != "end")
+            {
+                string nameOfPerson = inputTokens[0];
+                string camperModel = inputTokens[1];
+                int nights = int.Parse(inputTokens[2]);
+
+                if (!campingData.ContainsKey(nameOfPerson))
+                {
+                    campingData.Add(nameOfPerson, new Dictionary<string, int>());
+                }
+                campingData[nameOfPerson][camperModel] = nights;
+                
+                inputTokens = Console.ReadLine().Split(' ').ToArray();
+            }
+
+            var orderedCampingData = campingData
+                .OrderBy(d => d.Key.Length)
+                .OrderByDescending(d => d.Value.Count());
+
+            foreach (var camperdata in orderedCampingData)
+            {
+                string person = camperdata.Key;
+                var personsData = camperdata.Value;
+
+                int sum = personsData.Sum(d => d.Value);
+
+                Console.WriteLine(person + ": " + personsData.Count);
+
+                foreach (var personData in personsData)
+                {
+                    string innerKey = personData.Key;
+                    int innerValue = personData.Value;
+
+                    Console.WriteLine("***{0}", innerKey);
+                }
+                Console.WriteLine("Total stay: {0} nights", sum);
+            }
+        }
+    }
+}
